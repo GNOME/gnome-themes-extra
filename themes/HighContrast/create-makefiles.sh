6629784beb0b71d4c6e8efcab700379637352554
Marty E. Plummer <hanetzer@startmail.com>
2018-09-17 23:17:01 -0500

    treewide: remove autotools build files
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- themes/HighContrast/create-makefiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILE=Makefile-icons.am
-
-echo "NULL=" > $FILE
-echo "EXTRA_DIST = \\" >> $FILE
-find . \( -name "*.png" -or -name "*.svg" \) -print | awk '{ print $$0, " \\" }' | sort >> $FILE
-echo "\$(NULL)" >> $FILE
